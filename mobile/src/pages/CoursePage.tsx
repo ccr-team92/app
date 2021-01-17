e4496cfcc9339fd3ac10d1ace230e8e63ff113bf
Anibal Solon <anibalsolon@gmail.com>
2021-01-16 21:42:05 -0500

    image to components

--- mobile/src/pages/CoursePage.tsx
+++ mobile/src/pages/CoursePage.tsx
@@ -4,30 +4,34 @@ import {ScrollView, View, StyleSheet, Text, TouchableOpacity, Image, Dimensions,
 export default function Course() {
     return (
       <ScrollView style={styles.container}>
-        <ImageBackground source={require('./assets/bgCurso.png')} style={styles.header}>
+        <ImageBackground source={require('../assets/bgCurso.png')} style={styles.header}>
           <Text style={styles.titleCourse}>Descubra seus objetivos</Text>
         </ImageBackground>
         <View style={styles.containerFix}>
           <View style={styles.containerMain}>
-            <TouchableOpacity>
-              <Image source={require('./assets/progressBar.png')} style={{width: 314, height:14, marginTop: 10}}/>
-              <Image source={require('./assets/courseStats.png')} style={{width: 145, height:13, marginTop: 10}}/>
-            </TouchableOpacity>
+            <View style={styles.progress}>
+                <View style={styles.progressProgress}></View>
+                <Text style={styles.progressText}>25% CONCLUÍDO</Text>
+            </View>
+            <View style={styles.progressInfoBar}>
+                <View style={styles.progressInfo}><Image source={require('../assets/trophy.png')} style={{width: 13, height:11}}/><Text style={styles.progressInfoText}>3 MEDALHAS</Text></View>
+                <View style={styles.progressInfo}><Image source={require('../assets/time.png')} style={{width: 11, height:11}}/><Text style={styles.progressInfoText}>8H RESTANTES</Text></View>
+            </View>
             <View style={styles.line}></View>
             <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</Text>
-            <ImageBackground source={require('./assets/rectCourseContent.png')} style={styles.headerContent}>
-              <Image source={require('./assets/microphone.png')} style={{width: 49, height:49}}/>
+            <ImageBackground source={require('../assets/rectCourseContent.png')} style={styles.headerContent}>
+              <Image source={require('../assets/microphone.png')} style={{width: 49, height:49}}/>
               <Text style={styles.titleContent}>VÍDEO: Como definir objetivos?</Text>
             </ImageBackground>
             <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</Text>
-            <ImageBackground source={require('./assets/rectCourseContent.png')} style={styles.headerContent}>
-              <Image source={require('./assets/play.png')} style={{width: 49, height:49}}/>
+            <ImageBackground source={require('../assets/rectCourseContent.png')} style={styles.headerContent}>
+              <Image source={require('../assets/play.png')} style={{width: 49, height:49}}/>
               <Text style={styles.titleContent}>PODCAST: O que é um objetivo?</Text>
             </ImageBackground>
-            <Image style={styles.image} source={require('./assets/studying.png')} />
+            <Image style={styles.image} source={require('../assets/studying.png')} />
             <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  sed do eiusmod tempor incididunt ut,  sed do eiusmod tempor incididunt ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam  sed do eiusmod tempor incididunt ut,  sed do eiusmod tempor incididunt ut.</Text>
-            <ImageBackground source={require('./assets/rectCourseContent.png')} style={styles.headerContent}>
-              <Image source={require('./assets/download.png')} style={{width: 49, height:49}}/>
+            <ImageBackground source={require('../assets/rectCourseContent.png')} style={styles.headerContent}>
+              <Image source={require('../assets/download.png')} style={{width: 49, height:49}}/>
               <Text style={styles.titleContent}>DOWNLOAD: Metodologia OBJ</Text>
             </ImageBackground>
             <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</Text>
@@ -38,11 +42,53 @@ export default function Course() {
 }
 
 const styles = StyleSheet.create({
+    progress: {
+        backgroundColor: 'rgba(254,197,196,0.65)',
+        borderRadius: 10,
+        width: '100%',
+        padding: '1%',
+        position: 'relative',
+        marginVertical: 10,
+    },
+    progressProgress: {
+        backgroundColor: 'rgba(255,132,65,1)',
+        borderRadius: 10,
+        width: '25%',
+        left: 0,
+        top: 0,
+        bottom: 0,
+        position: 'absolute',
+        margin: 3,
+        zIndex: 9,
+    },
+    progressText: {
+        width: '100%',
+        left: 0,
+        textAlign: 'center',
+        fontFamily: 'Dosis_700Bold',
+        fontSize: 10,
+        zIndex: 10,
+    },
+    progressInfoBar: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'center',
+    },
+    progressInfo: {
+        flexDirection: 'row',
+        padding: 3,
+    },
+    progressInfoText: {
+        fontFamily: 'Dosis_400Regular',
+        fontSize: 10,
+        marginLeft: 5,
+    },
     container: {
         flex: 1,
         height: Dimensions.get('window').height,
         width: Dimensions.get('window').width,
         backgroundColor: '#fff',
+        paddingBottom: 30,
     },
     containerFix: {
       alignItems: 'center',
